9edf60bfe7230f28c25e716a81430756d6867a8b
hakalaja <hakalaja@msu.edu>
2025-02-27 21:15:51 -0500

    build pdf and add pie chart

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ import numpy as np
 import requests
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
 
 def authenticate_sheets(api_key):
     return build('sheets', 'v4', developerKey=api_key).spreadsheets()
+
+def build_pdf(date):
+    # Set up PDF canvas
+    c = canvas.Canvas("reports/" + date.replace(" ", "") + "Summary.pdf", pagesize=letter)
+    width, height = letter
+
+    # Add content
+    c.drawString(200, 700, "Summary Report for " + date)
+    c.drawImage("plots/pie_chart.png", 100, 450, width=344, height=235, mask='auto')
+
+    c.save()
 
 def main():
     # Load .env and retrieve variables
@@ -15,9 +28,10 @@ def main():
     SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
     # date = input("Enter a month to retrieve data (format '<Month> <year>', 'February 2025' for example):\n")
     date = "February 2025"
-    RANGE = date + os.getenv("RANGE")
     if date == "February 2025":
         RANGE = "February 2025!A2:C13"
+    else:
+        RANGE = date + os.getenv("RANGE")
 
     # Get spreadsheet data
     sheets = authenticate_sheets(API_KEY)
@@ -43,6 +57,9 @@ def main():
     plt.pie(spend, labels=labels, autopct='%1.1f%%')
     plt.tight_layout()
     plt.savefig("plots/pie_chart.png", bbox_inches='tight')
+    plt.clf()
+
+    build_pdf(date)
     
     return 0
 
---
+++ reports/README.md
@@ -0,0 +1,3 @@
+## This directory will remain empty.
+
+No plots will be uploaded to Git, they are stored locally.
